71d211b7ea88dea2abd9edcb5659b1ad8e808c39
Paul Johnson <paulie@doc.hillvalley>
2017-05-06 13:51:35 -0600

    removing legacy cdate code

--- release/src/api/pworg_api_getdate.pl
+++ release/src/api/pworg_api_getdate.pl
@@ -10,16 +10,12 @@
 
 sub pworg_api_getdate()
 {
-    ($file,$cdate) = @_;
+    ($file) = @_;
+    
     @abbr = qw( January February March April May June July August September
                         October November December );
     $filename = $file;
-
-    if ($cdate) {
-        $filename =~ s/.*com.//;
-    } else {
-        $filename =~ s/\..*//;
-    }
+    $filename =~ s/\..*//;
 
     ($sec,$min,$hour,$mday,$mon,$y,$wday,$yday,$isdst)=localtime($filename);
     $y += 1900;
